d3d4c75a127f1662d029d53dce56f9847699faad
Alexander Minyushkin <alexander.miniushkin@yandex.ru>
2016-06-02 15:32:26 +0300

    Language switch for bot added

--- hpqw/bot_brain.py
+++ hpqw/bot_brain.py
@@ -3,12 +3,14 @@ import telepot
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy 
 def _(x): return unicode(ugettext_lazy(x))
+from django.utils import translation
 #def _(x): return ugettext_lazy(x)
 #def _(x): return x
 from datetime import timedelta
 from random import randint
-from hpqw.models import Connection
+from hpqw.models import Connection, Language
 from hpqr.settings import bot as real_bot
+from hpqr.settings import LANGUAGES_SET
 
 
 msg = {u'chat': {u'first_name': u'Nick',
@@ -27,10 +29,10 @@ msg = {u'chat': {u'first_name': u'Nick',
 def get_print_link(con, host_name):
     return host_name + "/print/" + str(con.id) + "." + str(con.pin)
 
-test_lang_ru_to_en_msg = _("Тест с Русского на Английский")
-test_lang_ru_to_ru_msg = _("Тест с Русского на Русский")
-test_lang_en_to_en_msg = _("Test from English to English")
-test_lang_en_to_ru_msg = _("Test from English to Russian")
+test_lang_ru_to_en_msg = _(u"Тест с Русского на Английский")
+test_lang_ru_to_ru_msg = _(u"Тест с Русского на Русский")
+test_lang_en_to_en_msg = _(u"Test from English to English")
+test_lang_en_to_ru_msg = _(u"Test from English to Russian")
 
 help_text=(_('Hello! I am Hot Parking Bot. I understand commands:\n\n') +
            _('/make - create QR-code for your car.\n') +
@@ -38,15 +40,39 @@ help_text=(_('Hello! I am Hot Parking Bot. I understand commands:\n\n') +
            _('/ls - list all active QR-codes.\n') +
            _('/del id=XXX - delete code with id=XXX.\n') 
            )
+           
+def get_user_lang(chat_id):
+    x = Language.objects.filter(telegram_id=chat_id)
+    if (len(x) == 1):
+        return x[0].prefix        
+    return 'ru'
+    
+def set_user_lang(chat_id, lang):
+    obj, created = Language.objects.update_or_create(telegram_id=chat_id, prefix = lang)
+    pass
 
 def read_msg(msg = msg, bot = real_bot, host_name = "http://127.0.0.1:8000", _ = _):
 
     content_type, chat_type, chat_id = telepot.glance(msg)
     print "read_msg, " + str(chat_id) + ", " + str(content_type)
+    
+    translation.activate(get_user_lang(chat_id))
+    
     if content_type != 'text':
         bot.sendMessage(chat_id, _('Sorry, I understand only text.'))
         return
-    text = msg['text']    
+    text = msg['text']  
+    if text.startswith("/lang_"):
+        lang = text[6:]
+        if lang not in LANGUAGES_SET:
+            bot.sendMessage(chat_id, _("Sorry, we don't support this language."))
+            return
+        
+        set_user_lang(chat_id, lang)
+        translation.activate(get_user_lang(chat_id))
+        bot.sendMessage(chat_id, _("Now it's better."))
+        return
+    
     if text == '/make':                 # Create new code
         if Connection.objects.filter(telegram_id=chat_id).count() >=10:
             bot.sendMessage(chat_id, _('Sorry, you have too many registred codes.'))
--- hpqw/tests.py
+++ hpqw/tests.py
@@ -1,8 +1,10 @@
+# coding: utf-8
 from django.test import TestCase
 import hpqw.bot_brain as brain
-from hpqw.models import Connection
+from hpqw.models import Connection, Language
 from hpqr.settings import LANGUAGES_SET
 from django.utils.translation import ugettext_lazy
+def _(x): return unicode(ugettext_lazy(x))
 from django.utils import translation
 
 # Create your tests here.
@@ -35,6 +37,9 @@ class BotBrainTests(TestCase):
         Answer to unrecognizable message should contain 'Hello' - beginning of help message.
         No changes in Connection table should be made.
         """
+        cur_language = translation.get_language()
+        translation.activate('en')  
+        
         test_bot = BotStab()
         test_host = 'test.host'
         
@@ -55,8 +60,11 @@ class BotBrainTests(TestCase):
         
         chat_id, msg = test_bot.result()
         
+        print msg
         self.assertEqual('hello' in msg.lower(), True)
         self.assertEqual(size_before, Connection.objects.all().count())
+        
+        translation.activate(cur_language) 
         
     def test_make_one(self):
         """
@@ -233,7 +241,7 @@ class BotBrainTests(TestCase):
         """  
         cur_language = translation.get_language()
         translation.activate('en')        
-        self.assertEqual( brain.test_lang_ru_to_en_msg = , "Test from Russian to English")
+        self.assertEqual( _(brain.test_lang_ru_to_en_msg), u"Test from Russian to English")
         translation.activate(cur_language)
         
     def test_lang_ru_to_ru(self):
@@ -242,7 +250,7 @@ class BotBrainTests(TestCase):
         """     
         cur_language = translation.get_language()
         translation.activate('ru')        
-        self.assertEqual( brain.test_lang_ru_to_ru_msg = , "Тест с Русского на Русский")
+        self.assertEqual( _(brain.test_lang_ru_to_ru_msg), u"Тест с Русского на Русский")
         translation.activate(cur_language)
         
     def test_lang_en_to_en(self):
@@ -251,7 +259,7 @@ class BotBrainTests(TestCase):
         """  
         cur_language = translation.get_language()
         translation.activate('en')        
-        self.assertEqual( brain.test_lang_en_to_en_msg = , "Test from English to English")
+        self.assertEqual( _(brain.test_lang_en_to_en_msg), u"Test from English to English")
         translation.activate(cur_language)
         
     def test_lang_en_to_ru(self):
@@ -260,5 +268,91 @@ class BotBrainTests(TestCase):
         """     
         cur_language = translation.get_language()
         translation.activate('ru')        
-        self.assertEqual( brain.test_lang_en_to_ru_msg = , "Тест с Английского на Русский")    
-        translation.activate(cur_language)        
+        self.assertEqual( _(brain.test_lang_en_to_ru_msg), u"Тест с Английского на Русский")    
+        translation.activate(cur_language)  
+
+    def test_default_user_lang(self):
+        """
+        Verify that default language is Russian
+        """          
+        self.assertEqual(brain.get_user_lang(1), 'ru')
+
+    def test_updated_user_lang(self):
+        """
+        Verify that default language can be changed
+        """          
+        self.assertEqual(brain.get_user_lang(1), 'ru')
+        brain.set_user_lang(1, 'en')
+        self.assertEqual(brain.get_user_lang(1), 'en')
+
+    def test_updated_1_user_lang(self):
+        """
+        Verify that language changed only for one user
+        """          
+        self.assertEqual(brain.get_user_lang(1), 'ru')
+        self.assertEqual(brain.get_user_lang(2), 'ru')
+        brain.set_user_lang(1, 'en')
+        brain.set_user_lang(2, 'en')
+        self.assertEqual(brain.get_user_lang(1), 'en')
+        self.assertEqual(brain.get_user_lang(2), 'en')
+        
+        brain.set_user_lang(2, 'ru') # Changed only for one
+        
+        self.assertEqual(brain.get_user_lang(1), 'en') # This one stay the same
+        self.assertEqual(brain.get_user_lang(2), 'ru') # This one changed
+    
+    def test_set_lang_via_brain(self):
+        """
+        Change language
+        Language size should increase by one
+        """
+        test_bot = BotStab()
+        test_host = 'test.host'
+        
+        size_before = Language.objects.all().count()
+        
+        test_msg = {u'chat': {u'first_name': u'Nick',
+                         u'id': 200490867,
+                         u'last_name': u'Lee',
+                         u'type': u'private'},
+               u'date': 1444723969,
+               u'from': {u'first_name': u'Nick',
+                         u'id': 200490867,
+                         u'last_name': u'Lee'},
+               u'message_id': 4015,
+               u'text': u'/lang_en'}
+        
+        brain.read_msg(test_msg, test_bot, test_host, lambda x: x)
+        
+        chat_id, msg = test_bot.result()
+        
+        self.assertEqual('better' in msg.lower(), True)
+        self.assertEqual(size_before+1, Language.objects.all().count())
+
+    def test_set_unsupported_lang_via_brain(self):
+        """
+        Try to change language to unsupported.
+        Language size should not change
+        """
+        test_bot = BotStab()
+        test_host = 'test.host'
+        
+        size_before = Language.objects.all().count()
+        
+        test_msg = {u'chat': {u'first_name': u'Nick',
+                         u'id': 200490867,
+                         u'last_name': u'Lee',
+                         u'type': u'private'},
+               u'date': 1444723969,
+               u'from': {u'first_name': u'Nick',
+                         u'id': 200490867,
+                         u'last_name': u'Lee'},
+               u'message_id': 4015,
+               u'text': u'/lang_never_support'}
+        
+        brain.read_msg(test_msg, test_bot, test_host, lambda x: x)
+        
+        chat_id, msg = test_bot.result()
+        
+        self.assertEqual('sorry' in msg.lower(), True)
+        self.assertEqual(size_before, Language.objects.all().count())    
