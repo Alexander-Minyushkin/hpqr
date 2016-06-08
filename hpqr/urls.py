d5bd2f90c6ccb41996f2cbfe462e7bfefd77a0c1
Alexander Minyushkin <alexander.minyushkin@yandex.ru>
2016-06-08 22:22:54 +0300

    Language support in templates

--- hpqr/urls.py
+++ hpqr/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from hpqw import views
 from django.conf.urls import handler400
@@ -6,20 +7,22 @@ from django.conf.urls import handler400
 from hpqr.settings import bot_token
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'hpqr.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(bot_token, views.telegram_hook),
-    url(r'^$', views.index),    
+    url(r'^robots\.txt$', views.robots),
+    #url(r'^admin/', include(admin.site.urls)),
+    ]
+
+urlpatterns += i18n_patterns( 
+    url(r'^$', views.index),  
+    url(r'^/$', views.index),   
     url(r'^([0-9]+)\.([0-9]{4})', views.connection),        
     url(r'^print/([0-9]+)\.([0-9]{4})', views.print_page),
     url(r'^register$', views.register),
     url(r'^credits$', views.credits),
     url(r'^contact$', views.contact),
-    url(r'^index$', views.index),
-    url(r'^robots\.txt$', views.robots),
-    #url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^index$', views.index)
+    )
+    
+
 
 handler400 = 'hpqw.views.bad_request'
